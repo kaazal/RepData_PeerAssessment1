c972c01d69da8000b1f423c04eafab56e5f1dabb
kaazal <43524019+kaazal@users.noreply.github.com>
2018-12-02 23:54:27 -0500

    Delete Imptedvalues.R

--- Imptedvalues.R
+++
@@ -1,7 +0,0 @@
-numberofmissingvalues <- sum(is.na(Activity$steps))
-activity2 <-  Activity
-na <- is.na(activity2$steps)
-Interval<- tapply(activity2$steps,activity2$interval,mean,na.rm=TRUE,simplify=TRUE)
-
-activity2$steps[na]  <-Interval[as.character(activity2$interval[na])]
-print(paste0("Number Of Missing Values : ", numberofmissingvalues))

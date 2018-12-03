91be0daadfcd0939b5a7565e522fc9ad378132b2
kaazal <43524019+kaazal@users.noreply.github.com>
2018-12-02 23:53:52 -0500

    Delete histogram.R

--- histogram.R
+++
@@ -1,8 +0,0 @@
-
-library(knitr)
-library(dplyr)
-Activity <- read.csv("activity.csv",header=TRUE)
-Totalsteps <- aggregate(steps ~ date,Activity,  sum)
-png("Hist_Totalsteps.png")
-hist(Totalsteps$steps, col="pink",xlab = "Frequency", ylab = "steps", main = "Total steps taken each day")
-dev.off()
